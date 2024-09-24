d7d711746e41dced7f4f3217ff2749ce342d4bf6
Adrian Navarro <2023_adrian.navarro@iticbcn.cat>
2024-09-24 17:47:00 +0200

    exercici6 acabat

---
+++ ACTIVITAT2/exercici6.py
@@ -0,0 +1,16 @@
+# LOOPS: Mostrar els números de l’1 al 100 amb un while. Fer el mateix exercici amb for.
+
+print("\nExercici amb While: \n")
+
+num = 1
+
+while num <= 100:
+    print(num)
+    num += 1
+
+print("\nExercici amb un for: \n")
+
+num = 1
+
+for num in range(1, 101):
+    print(num)
