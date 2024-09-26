e9dd5be831931bb449b8e3a66da3f266d60c27e1
Adrian Navarro <2023_adrian.navarro@iticbcn.cat>
2024-09-26 17:29:46 +0200

    exercici10 acabat

---
+++ ACTIVITAT2/exercici10.py
@@ -0,0 +1,34 @@
+import random
+
+# Crear un arxiu on caldrà endevinar el número escollit pel programa entre 1 i 100.
+# Cada vegada que l’usuari hi posi un número, caldrà indicar si és més petit o més gran fins que encerti
+# i el missatge haurà d’indicar que ha encertat. Indicar també el número d’intents.
+
+secretNumber = random.randint(1, 100)
+
+userInput = int(input("Introdueix un número: "))
+
+tries = 0
+
+isNumCorrect = False
+
+while not isNumCorrect:
+    while userInput < 1 or userInput > 100:
+        userInput = int(
+            input("Introdueix un valor entre el rang seleccionat (1 entre 100): ")
+        )
+
+    if userInput > secretNumber:
+        tries += 1
+        userInput = int(
+            input(f" {userInput} massa gran, indica nou número ({tries} intents): ")
+        )
+    elif userInput < secretNumber:
+        tries += 1
+        userInput = int(
+            input(f" {userInput} massa petit, indica nou número ({tries} intents): ")
+        )
+    else:
+        isNumCorrect = True
+        tries += 1
+        print(f"Enhorabona, has encertat. Número d'intents: {tries}")
