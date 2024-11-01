c6ba7da65d2e4224e52e2a78a16d96f07ee30917
Adrian Navarro <2023_adrian.navarro@iticbcn.cat>
2024-11-01 16:10:42 +0100

    nombre genero del libro solucionado

--- ACTIVITAT_7/create.py
+++ ACTIVITAT_7/create.py
@@ -1,27 +1,22 @@
 import connection as conn
 
 
-def create_record(table_name, title, author, gender, pages, price, editorial):
+def create_record(table_name, title, author, genre, pages, price, editorial):
     connection = conn.get_connection()
 
     if connection:
         try:
             cursor = connection.cursor()
 
-            # query = f"""
-            #     INSERT INTO {table_name} (name, last_name, age)
-            #     VALUES ({name}, {last_name}, {age})
-            # """
-
             query = f"""
-                INSERT INTO {table_name} (title, author, gender, pages, price, editorial) 
+                INSERT INTO {table_name} (title, author, genre, pages, price, editorial) 
                 VALUES (%s, %s, %s, %s, %s, %s)
             """
 
             # cursor.execute(query)
 
             # se lo paso con una tupla para evitar SQLInjection
-            cursor.execute(query, (title, author, gender, pages, price, editorial))
+            cursor.execute(query, (title, author, genre, pages, price, editorial))
 
             connection.commit()
 
